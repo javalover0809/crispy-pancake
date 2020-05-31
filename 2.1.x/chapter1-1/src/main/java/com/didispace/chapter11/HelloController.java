bd0d144ac4e85b9274780ec2d703f31601e69933
javalover0809 <280671026@qq.com>
2020-05-31 09:03:54 +0800

    first

--- 2.1.x/chapter1-1/src/main/java/com/didispace/chapter11/HelloController.java
+++ 2.1.x/chapter1-1/src/main/java/com/didispace/chapter11/HelloController.java
@@ -5,10 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World";
+        return "打开这个界面说明你已经打开了spring boot";
+    }
+
+    @RequestMapping("/mytest")
+    public String mytest() {
+        return "这是我的测试";
     }
 
 }
